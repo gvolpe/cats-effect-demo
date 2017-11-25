5d7d25bd55d21c3584d176475bbc94986a0eeb03
Gabriel Volpe <volpegabriel@gmail.com>
2017-11-25 11:25:10 +0200

    Http4s example abstracting effect type.

--- src/main/scala/com/github/gvolpe/effects/http/UsersHttpEndpoint.scala
+++ src/main/scala/com/github/gvolpe/effects/http/UsersHttpEndpoint.scala
@@ -1,36 +1,40 @@
 package com.github.gvolpe.effects.http
 
+import cats.Applicative
 import cats.effect._
+import cats.syntax.applicative._
 import cats.syntax.applicativeError._
+import cats.syntax.flatMap._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe._
-import org.http4s.client.dsl.Http4sClientDsl
-import org.http4s.dsl.io._
+import org.http4s.dsl.Http4sDsl
 
 import scala.concurrent.TimeoutException
 
 case class User(username: String, email: String)
 
-class UsersHttpEndpoint extends Http4sClientDsl[IO] {
+object UsersHttpEndpoint extends UsersHttpEndpoint[IO](new UserService[IO])
 
-  val usersErrorHandler: PartialFunction[Throwable, IO[Response[IO]]] = {
+class UsersHttpEndpoint[F[_] : Effect](userService: UserService[F]) extends Http4sDsl[F] {
+
+  val usersErrorHandler: PartialFunction[Throwable, F[Response[F]]] = {
     case e: TimeoutException => InternalServerError(e.getMessage)
   }
 
-  val service: HttpService[IO] = HttpService[IO] {
+  val service: HttpService[F] = HttpService[F] {
     case GET -> Root / "users" =>
-      val users: IO[List[User]] = UserService.findAll()
+      val users: F[List[User]] = userService.findAll()
       users.flatMap(u => Ok(u.asJson)).recoverWith(usersErrorHandler)
   }
 
 }
 
-object UserService {
+class UserService[F[_] : Applicative] {
 
-  def findAll(): IO[List[User]] = IO {
-    List(User("gvolpe", "gvolpe@github.com"), User("root", "root@unix.org"))
+  def findAll(): F[List[User]] = {
+    List(User("gvolpe", "gvolpe@github.com"), User("root", "root@unix.org")).pure[F]
   }
 
 }
